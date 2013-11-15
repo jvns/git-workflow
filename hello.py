036f0dcf541b0faa2a56d5fbdef61ac954043344
Julia Evans <julia@jvns.ca>
2013-11-14 23:32:13 -0500

    Add template

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template('index.html', name="Julia")
